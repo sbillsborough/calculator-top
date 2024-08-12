b87ea0f6e7cf32a04228b85a2dcf43064e8bc2aa
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-08-12 16:17:00 +0100

    displayValue broken

--- assets/scripts/inputFunctions.js
+++ assets/scripts/inputFunctions.js
@@ -8,10 +8,10 @@ let displayValue = "";
 // Functions to handle inputs
 export function handleNumberInput(number) {
   if (operator === "") {
-    num1 += number;
+    num1 = number;
     displayValue = num1;
   } else {
-    num2 += number;
+    num2 = number;
     displayValue = num1 + operator + num2;
   }
   updateDisplay(displayValue);
@@ -21,20 +21,27 @@ export function handleOperatorInput(op) {
   if (num1 !== "") {
     if (operator === "") {
       operator = op;
-      displayValue += operator;
+      displayValue = num1 + operator;
     } else if (num2 !== "") {
       handleEqualsInput();
       operator = op;
+      displayValue = num1 + operator;
     }
     updateDisplay(displayValue);
   }
 }
 
 export function handleDecimalInput() {
-  if (operator === "" && !num1.includes(".")) {
-    num1 += ".";
-  } else if (operator !== "" && !num2.includes(".")) {
-    num2 += ".";
+  if (operator === "") {
+    if (!num1.includes(".")) {
+      num1 += ".";
+      displayValue = num1;
+    }
+  } else {
+    if (!num2.includes(".")) {
+      num2 += ".";
+      displayValue = num2;
+    }
   }
   displayValue = num1 + operator + num2;
   updateDisplay(displayValue);
@@ -52,15 +59,15 @@ export function handleEqualsInput() {
 }
 
 export function handleDeleteInput() {
-  if (num2 !== "") {
-    num2 = num2.slice(0, -1);
-    displayValue = num1 + operator + num2;
-  } else if (operator !== "") {
+  if (num1 !== "" && num2 === "" && operator === "") {
+    num1 = "";
+    displayValue = "";
+  } else if (num1 !== "" && num2 === "" && operator !== "") {
     operator = "";
-    displayValue = num1;
-  } else if (num1 !== "") {
-    num1 = num1.slice(0, -1);
-    displayValue = num1;
+    displayValue = num1 + operator;
+  } else if (num1 !== "" && num2 !== "" && operator !== "") {
+    num2 = "";
+    displayValue = num1 + operator + num2;
   }
   updateDisplay(displayValue);
 }
